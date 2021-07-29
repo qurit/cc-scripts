005f44546d1c89267847a759de63c66f4158f0e6
Jiayi Tang <jiayitang10@gmail.com>
2021-07-28 23:13:49 -0600

    added Matlab file

---
+++ startup.m
@@ -0,0 +1,6 @@
+%% Add the PET Toolbox
+toolboxpath='~/projects/def-curibe/duetto';
+addpath(genpath(toolboxpath))
+distcomp.feature( 'LocalUseMpiexec', false )
+
+disp("Hello from Jiayi's /home/jtang1/MATLAB/startup.m :)")
